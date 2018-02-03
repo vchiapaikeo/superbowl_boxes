d7dfaa58009d0ba283f98bcd738165dee0a6a5ce
Victor Chiapaikeo <vchiapaikeo@fb.com>
2018-02-03 16:21:49 -0500

    [generate-boxes] Init

---
+++ generate_boxes.py
@@ -0,0 +1,69 @@
+import argparse
+import logging
+import numpy as np
+import sys
+
+from typing import List
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog=__name__,
+        description='Randomly generates superbowl boxes for '
+            'you and your friends.')
+
+    parser.add_argument(
+        '--friends',
+        type=str,
+	required=True,
+        help='Comma separated list of friend names.')
+
+    parser.add_argument(
+        '--num-boxes',
+        type=int,
+	default=100,
+        help='Comma separated list of friend names.')
+
+    return parser.parse_args()
+
+
+def format_results(results: List[str]) -> str:
+    formatted = ''
+    for i, name in enumerate(results):
+	# Newline every 10 names
+        num = i + 1
+        formatted += '{num}: {name} '.format(**locals())
+        if num % 10 == 0:
+            formatted += '\n'
+    return formatted
+
+
+def main(friends: List[str], num_boxes: int) -> None:
+    num_friends = len(friends)
+    results = np.random.choice(
+        friends,
+        num_boxes,
+        p=([1 / num_friends] * num_friends))
+
+    formatted_results = format_results(results)
+
+    logger.info(
+	'Final Results: \n'
+	'{}'.format(formatted_results))
+
+
+if __name__ == "__main__":
+    options = parse_args()
+    friends = [name.strip() for name in options.friends.split(',')]
+    logger.info('List of friends passed: {}'.format(friends))
+    main(friends, options.num_boxes)
+
